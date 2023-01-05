c85a5bb73c9825727b7a34aab6aa99fa3215842f
Bence Papp <bencepapp1101@gmail.com>
2023-01-05 17:28:18 +0100

    🚀 Converter component state and memo added for input and output

--- components/ui/Converter/Converter.tsx
+++ components/ui/Converter/Converter.tsx
@@ -1,12 +1,17 @@
-import { FC } from "react";
+import { FC, useMemo, useState } from "react";
 import s from "./Converter.module.css";
 import { Input } from "../../common";
 
 const Converter: FC = () => {
+  const [inputValue, setInputValue] = useState<number>(1);
+  const outputValue = useMemo<string>(() => {
+    return `${inputValue}`;
+  }, [inputValue]);
+
   return (
     <div>
       <Input />
-      <h2>output value</h2>
+      <h2>{outputValue}</h2>
     </div>
   );
 };
