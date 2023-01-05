7dbf36d1d02c5b0635cf17d0934fb2e3c996d978
Bence Papp <bencepapp1101@gmail.com>
2023-01-05 17:17:27 +0100

    🚀 Input component created for better reuseability

---
+++ components/common/Input/Input.tsx
@@ -0,0 +1,7 @@
+import s from "./Input.module.css";
+
+const Input = () => {
+  return <div>Enter</div>;
+};
+
+export default Input;
--- components/ui/Converter/Converter.tsx
+++ components/ui/Converter/Converter.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import s from "./Converter.module.css";
+import { Input } from "../../common";
 
 const Converter: FC = () => {
   return (
     <div>
-      <input type="text" />
+      <Input />
       <h2>output value</h2>
     </div>
   );
