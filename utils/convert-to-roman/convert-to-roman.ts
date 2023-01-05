68a594f6bb0ae94ac7d86f40c2c6fff069131e1a
Bence Papp <bencepapp1101@gmail.com>
2023-01-05 17:56:50 +0100

    🔧 convertToRoman error handling to avoid possible errors

--- utils/convert-to-roman/convert-to-roman.ts
+++ utils/convert-to-roman/convert-to-roman.ts
@@ -1,4 +1,9 @@
 const convertToRoman = (rawValue: number): string => {
+  if (rawValue < 1 || rawValue > 1000)
+    return "Input value must be between 0 and 1000!";
+
+  if (!Number.isInteger(rawValue)) return "Input value must be an integer!";
+
   return `${rawValue}`;
 };
 
