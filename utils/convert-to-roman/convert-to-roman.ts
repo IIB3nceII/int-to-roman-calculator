9cc89ad595b7ddd1ab30a978e0497268d45410f1
Bence Papp <bencepapp1101@gmail.com>
2023-01-05 18:08:41 +0100

    🔧 convertToRoman lookup table crated for roman and arabic number pairs

--- utils/convert-to-roman/convert-to-roman.ts
+++ utils/convert-to-roman/convert-to-roman.ts
@@ -4,7 +4,29 @@ const convertToRoman = (rawValue: number): string => {
 
   if (!Number.isInteger(rawValue)) return "Input value must be an integer!";
 
-  return `${rawValue}`;
+  let romanValue = "";
+
+  const lookupTable: { [key: string]: number } = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  for (const key in lookupTable) {
+    console.log(lookupTable[key]);
+  }
+
+  return romanValue;
 };
 
 export default convertToRoman;
