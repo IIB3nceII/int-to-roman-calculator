2948bb52a461722480c73817b12264be7187efcf
Bence Papp <bencepapp1101@gmail.com>
2023-01-05 17:24:53 +0100

    🚀 Input component props and interface added, optional label

--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -1,7 +1,18 @@
+import { FC, InputHTMLAttributes } from "react";
 import s from "./Input.module.css";
 
-const Input = () => {
-  return <div>Enter</div>;
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  id?: string;
+  label?: string;
+}
+
+const Input: FC<IInputProps> = ({ id, label, ...rest }) => {
+  return (
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input id={id} {...rest} />
+    </>
+  );
 };
 
 export default Input;
